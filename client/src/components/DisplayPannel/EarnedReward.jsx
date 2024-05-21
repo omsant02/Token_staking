523a2c967cf17dedca804bd2d1daa420064cdf6e
omsant02 <omsantoshwar02@gmail.com>
2024-05-21 15:18:30 +0530

    display earned reward

--- client/src/components/DisplayPannel/EarnedReward.jsx
+++ client/src/components/DisplayPannel/EarnedReward.jsx
@@ -1,7 +1,37 @@
-import React from "react";
+import { useState, useContext, useEffect } from "react";
+import { ethers } from "ethers";
+import web3Context from "../../context/Web3Context";
 
 const EarnedReward = () => {
-  return <div></div>;
+  const { stakingContract, selectedAccount } = useContext(web3Context);
+  const [rewardVal, setRewardVal] = useState("0");
+
+  useEffect(() => {
+    const fetchStakeRewardInfo = async () => {
+      try {
+        //fetching earned amount of a user
+        const rewardValueWei = await stakingContract.earned(selectedAccount);
+        const rewardValueEth = ethers
+          .formatUnits(rewardValueWei, 18)
+          .toString();
+        const roundedReward = parseFloat(rewardValueEth).toFixed(2);
+        console.log(roundedReward);
+        setRewardVal(roundedReward);
+      } catch (error) {
+        toast.error("Error fetching the reward:");
+        console.error(error.message);
+      }
+    };
+
+    stakingContract && fetchStakeRewardInfo();
+  }, [stakingContract, selectedAccount]);
+
+  return (
+    <div className="earned-reward">
+      <p>Earned Reward:</p>
+      <span>{rewardVal}</span>
+    </div>
+  );
 };
 
 export default EarnedReward;
